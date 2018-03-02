1fbf333a4d1b3e689d5b93afb25119a54a2f6d8d
alan <2818083494@qq.com>
2018-03-02 10:56:39 +0800

    Initial commit

---
+++ src/test/java/tk/mybatis/springboot/ga.java
@@ -0,0 +1,19 @@
+package tk.mybatis.springboot;
+
+/**
+ *
+ */
+public class ga {
+    /**
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+    public static void dfdf(){
+        System.out.println(11);
+        System.out.println();
+
+    }
+
+}
